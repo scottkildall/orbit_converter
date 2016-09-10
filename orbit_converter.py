301ed7c5d47251208d7e357fa858da135ab3e2c2
Scott Kildall <scott@kildall.com>
2016-09-10 11:48:48 -0700

    added stub, gitignore

---
+++ orbit_converter.py
@@ -0,0 +1,12 @@
+#! /user/bin/env python
+import sys
+import datetime
+
+
+# ProcessFiles() is the main entry point
+def Process():
+	print "processing files"
+
+
+if __name__ == "__main__":
+	Process()
