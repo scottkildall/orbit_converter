0be86914d487e09cd49a8992102348dce0e380c7
Scott Kildall <scott@kildall.com>
2016-09-11 21:02:06 -0700

    exaction of start/stop date

--- orbit_converter.py
+++ orbit_converter.py
@@ -32,7 +32,22 @@ def processEphemerisFile(filename):
 	if index > -1:
 		targetBodyName = extractTargetBodyName(fileText, index, searchStr)
 	
+	searchStr = "Start time      : A.D."
+	index = fileText.find(searchStr)
+	if index > -1:
+		startDate = extractStartOrStopDate(fileText, index, searchStr)
+	
+	searchStr = "Stop  time      : A.D."
+	index = fileText.find(searchStr)
+	if index > -1:
+		stopDate = extractStartOrStopDate(fileText, index, searchStr)
+	
+	# add step size here??
+	# Step-size       : 
+
 	print targetBodyName
+	print startDate
+	print stopDate
 
 # Line in file looks something like this:
 # Target body name: 26858 Misterrogers (1993 FR)
@@ -45,6 +60,30 @@ def extractTargetBodyName(fileText, index, searchStr):
 			break
 
 	return targetBodyName
+
+# Line in file looks something like this:
+# Start time      : A.D. 2100-Jan-01 00:00:00.0000 TDB
+def extractStartOrStopDate(fileText, index, searchStr):
+	dateStr = "002 Not Found"
+	startIndex = -1
+	endIndex = -1
+
+	for i in range(index + len(searchStr), len(fileText)):
+		if fileText[i] == " ":
+			if startIndex > 0:
+				endIndex = i
+				dateStr = fileText[startIndex:endIndex]
+				break
+			else:
+				# first ' ' character
+				startIndex = i+1
+
+
+
+
+	# now dateStr = "2100-Jan-01" (or something like this), swap around to make "Jan 01 2001"
+	dateStr = dateStr[5:8] + " " + dateStr[9:11] + " " + dateStr[0:4] 
+	return dateStr
 
 # makes an array from lines in a file, stripping off the newlines
 def getFileLines(filename):
